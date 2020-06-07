a29897cdf73a38d2eeb344b6a2a22adf464c56bd
HafsaAhmed80 <mehafsaahmed87@gmail.com>
2020-06-07 07:42:38 -0700

    java assignment

---
+++ app.js
@@ -0,0 +1,114 @@
+//Chapter No 1
+alert("Welcome to my website");
+
+alert("Error! Please enter a valid password")
+
+alert("Welcome to JS land.. \nhappy Coding!") 
+
+
+alert("Welcome to JS land.. ")
+
+alert("happy coding!\n ")
+
+alert("Hello... I can run JS through my web browser's console");
+
+//chapter no 2
+var username = "Hafsa";
+    var myName = "Hafsa Ahmed "
+    var message = "Hello World";
+    var age = 20;
+    var profession = "Certifies Mobile Application Developer";
+    var pizza = "Pizza";
+    var email = "mehafsaahmed87@gmail.com";
+    var book = "A smarter way to learn JavaScript";
+    var design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+
+    alert("User Name : "+username);
+    alert("My name is : "+myName);
+    alert("I am "+age+" years old");
+    alert(profession);
+    alert(pizza+"\n"+pizza.slice(0,pizza.length-1)+"\n"+pizza.slice(0,pizza.length-2)+"\n"+pizza.slice(0,pizza.length-3)+"\n"+pizza.slice(0,pizza.length-4));
+    alert(email);
+    alert(book);
+    alert(design)
+
+
+    //chapter no 03
+
+    var age = 20;
+    var birthYear = 2000;
+    var visitTime = 14;
+    var url= "www.xyzClothing.com";
+    var visitorName = "Hafsa Ahmed";
+    var quantity = 15 ;
+    alert("I am "+age+" year old");
+    alert("You have visit this site "+visitTime+" times")
+    document.write("My birth year is "+birthYear+"<br>The data type of my declared variable  is Number<br><br><b>"+visitorName+"</b> ordered <b>"+quantity+" T-Shirts</b> on" +"XYZ clothing store");
+
+//chapter no 4
+
+
+var Strings,number,illegal
+document.write("<h1>Legal variable:</h1><br>");
+document.write( "hafsa","_characters","$number","_123","variable12");
+document.write("<h1>ILLegal variable:</h1><br>");
+document.write("'1hafsa','-12', 'Variable 1' '1stName','@name','var+1','.name'<br>");
+document.write("Variable names can only contain letters, numbers,underscores, and dollar signs. <br>");
+document.write("For example $my_1stVariable <br>");
+document.write("<h1>Rules for naming JS variables:</h1><br>")
+document.write("Variable names can only contain letter, numbers, $ and _.<br>");
+document.write("For example $my_1stVariable<br>");
+document.write("Variable names are case sensitive");
+document.write("Variable names should not be JS Keyword<br>");
+
+
+//chapter no 5
+
+var num1 = prompt("Enter 1st  number;");
+    var num2 = prompt("Enter 1st  number;");
+    var result = Number(num1)+Number(num2);
+    document.write(Number(num1)+"+"+Number(num2)+"="+result+"<br>");
+    result = Number(num1)-Number(num2);
+    document.write(Number(num1)+"-"+Number(num2)+"="+result+"<br>");
+    result = Number(num1)*Number(num2);
+    document.write(Number(num1)+"*"+Number(num2)+"="+result+"<br>");
+    result = Number(num1)/Number(num2);
+    document.write(Number(num1)+"/"+Number(num2)+"="+result+"<br>");
+    result = Number(num1)%Number(num2);
+    document.write(Number(num1)+"%"+Number(num2)+"="+result+" Modulus<br> ");
+
+    var newVar;
+
+    document.write("Value after valraible declaration is <b>"+newVar+"<b><br>");
+    newVar = 6;
+    document.write("Initial value: <b>"+newVar+"<b><br>");
+    var increment = newVar++;
+    document.write("Value after increment is <b>"+newVar+"<b><br>");
+    var addition = newVar + increment;
+    document.write("Value after addition is <b>"+addition+"<b><br>");
+    var decrement = addition - 1;
+    document.write("Value after decrement is <b>"+decrement+"<b><br>");
+    document.write("Value remainder is <b>"+newVar%newVar+"<b><br>");
+
+    var ticket = 600;
+    document.write("The Cost to buy 5 tickets to a movie is "+ticket*5+" PKR<br>");
+
+    var table = 4;
+    document.write("<b>Tabel of "+table+"</b><br>")
+    document.write(table+"X 1 ="+table*1+"<br>");
+    document.write(table+"X 2 ="+table*2+"<br>");
+    document.write(table+"X 3 ="+table*3+"<br>");
+    document.write(table+"X 4 ="+table*4+"<br>");
+    document.write(table+"X 5 ="+table*5+"<br>");
+    document.write(table+"X 6 ="+table*6+"<br>");
+    document.write(table+"X 7 ="+table*7+"<br>");
+    document.write(table+"X 8 ="+table*8+"<br>");
+    document.write(table+"X 9 ="+table*9+"<br>");
+    document.write(table+"X 10 ="+table*10+"<br>");
+
+    //chapter no 6
+
+    
+
+
+
